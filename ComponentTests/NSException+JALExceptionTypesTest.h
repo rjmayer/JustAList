5097519d6a461b739805c6f8bb00e1628b737237
rich <rjmayer@gmail.com>
2013-06-25 02:08:04 +0200

    still need another commit

---
+++ ComponentTests/NSException+JALExceptionTypesTest.h
@@ -0,0 +1,13 @@
+//
+//  NSException+JALExceptionTypesTest.h
+//  JustAList
+//
+//  Created by Richard Mayer on 25/06/2013.
+//  Copyright (c) 2013 Richard Mayer. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSException_JALExceptionTypesTests : SenTestCase
+
+@end
